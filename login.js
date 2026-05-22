c304d3b49c6af789d37fcc45720caefc3f911e69
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-22 00:10:13 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,40 @@
 const loginForm = document.getElementById("loginForm")
 
-loginForm.addEventListener("submit",(e)=>{
+loginForm.addEventListener("submit", async (e)=>{
 
 e.preventDefault()
 
 const email = document.getElementById("loginEmail").value
+
 const password = document.getElementById("loginPassword").value
 
-localStorage.setItem("studentEmail",email)
+const response = await fetch("YOUR_WEB_APP_URL",{
 
-window.location.href="dashboard.html"
+method:"POST",
+
+body:JSON.stringify({
+action:"login",
+email:email,
+password:password
+})
+
+})
+
+const result = await response.json()
+
+if(result.status == "success"){
+
+localStorage.setItem(
+"student",
+JSON.stringify(result.student)
+)
+
+window.location.href = "dashboard.html"
+
+}else{
+
+alert("Invalid Email or Password")
+
+}
 
 })
